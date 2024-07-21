ca68d9b2756e79e3420e912af4f33ac577577e16
vahan <sahakyanvahan@gmail.com>
2024-07-21 20:57:22 +0200

    Implement platform icon list

---
+++ src/components/PlatformIconList.tsx
@@ -0,0 +1,50 @@
+import {
+  FaWindows,
+  FaPlaystation,
+  FaXbox,
+  FaApple,
+  FaLinux,
+  FaAndroid,
+} from "react-icons/fa";
+import { MdPhoneIphone } from "react-icons/md";
+import { SiNintendo } from "react-icons/si";
+import { BsGlobe } from "react-icons/bs";
+import { Platform } from "../hooks/useGames";
+import {
+  Card,
+  CardBody,
+  HStack,
+  Heading,
+  Icon,
+  Image,
+  Text,
+} from "@chakra-ui/react";
+import { IconType } from "react-icons";
+
+interface Props {
+  platforms: Platform[];
+}
+
+const PlatformIconList = ({ platforms }: Props) => {
+  const IconMap: { [key: string]: IconType } = {
+    pc: FaWindows,
+    playstation: FaPlaystation,
+    xbox: FaXbox,
+    nintendo: SiNintendo,
+    mac: FaApple,
+    linux: FaLinux,
+    androind: FaAndroid,
+    iphone: MdPhoneIphone,
+    web: BsGlobe,
+  };
+
+  return (
+    <HStack marginY={1}>
+      {platforms.map((platform) => (
+        <Icon as={IconMap[platform.slug]} color="gray.500" />
+      ))}
+    </HStack>
+  );
+};
+
+export default PlatformIconList;
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,11 +2,18 @@ import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
 
+interface Platform{
+    id: number;
+    name: string;
+    slug: string
+}
+
 export interface Game {
     id: number;
     slug: string;
     name: string;
     background_image: string;
+    parent_platforms: { platform: Platform }[]
 }
 
 interface GameResponse {
